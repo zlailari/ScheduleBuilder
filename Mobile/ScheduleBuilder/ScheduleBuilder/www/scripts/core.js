ca4f0e6df11fcb1c8a388dbc3e1697d668d425ee
Chris Woodle <chris@droplit.io>
2015-10-10 21:31:20 -0400

    mobile

--- Mobile/ScheduleBuilder/ScheduleBuilder/www/scripts/core.js
+++ Mobile/ScheduleBuilder/ScheduleBuilder/www/scripts/core.js
@@ -1,7 +1,13 @@
 "use strict";
-var cvm = new courseViewModel();
+
+var cvm = new scheduleViewModel();
+
+var data = coursesArray;
+
 var colorIndex = 0;
 var formOpen = ko.observable(false);
+var schedules = ko.observableArray();
+
 var colors = colors = [
         { color: "Cobalt" },
         { color: "Crimson" },
@@ -12,12 +18,13 @@ var colors = colors = [
         { color: "Green" },
         { color: "Red" },
         { color: "Steel" }
-
-
 ]
-function courseViewModel() {
+
+function scheduleViewModel() {
     var self = this;
-    self.courses = ko.observableArray();
+    //self.courses = ko.observableArray();
+    self.courseList = [];
+    self.schedules = ko.observableArray();
     self.scalingFactor = ko.observable();
     self.scalingFactor(30);
     self.days = [
@@ -27,8 +34,8 @@ function courseViewModel() {
         { day: "Thu", abb: "R" },
         { day: "Fri", abb: "F" },
         { day: "Sat", abb: "S" },
-        {day : "Sun", abb:"X"}
-    ]
+        { day: "Sun", abb: "X" }
+    ];
     self.times = [
         { time: 800 },
         { time: 900 },
@@ -45,12 +52,24 @@ function courseViewModel() {
         { time: 2000 },
         { time: 2100 },
         { time: 2200 }
-
-
-
-    ]
+    ];
 }
-function course(title,id, startTime, endTime, days) {
+function scheduleObject() {
+    var self = this;
+    self.courses = ko.observableArray();
+}
+
+
+function lookup(CRN) {
+    for (var i = 0; i < data.length; i++) {
+        if (data[i].CRN == CRN) {
+            return data[i];
+        }
+    }
+    return 'NONE'
+}
+
+function course(title, id, startTime, endTime, days) {
     var self = this;
     self.title = ko.observable(title);
     self.id = ko.observable(id);
@@ -60,21 +79,22 @@ function course(title,id, startTime, endTime, days) {
     self.height = ko.observable(self.endTime() - self.startTime());
     self.color = colors[colorIndex];
     colorIndex++;
+    /*
     for (var index = 0; index < cvm.courses().length; index++) {
         if (self.id == cvm.courses()[index].id) {
             self.color = cvm.courses()[index].color;
             colorIndex--;
         }
-    }
-    
-    
+    }*/
+
+
 }
 $(document).ready(function () {
-    ko.applyBindings(cvm,document.getElementById('target'), new courseViewModel());
+    ko.applyBindings(cvm, document.getElementById('target'));
 
     $(document).on('click', '#AddClassButton', function () {
         readForm();
-       
+
     });
 
     $(document).on('click', '#addCourse', function () {
@@ -82,24 +102,68 @@ $(document).ready(function () {
     });
 
     $(document).on('click', '#close', function () {
-       closeForm();
+        closeForm();
     });
 
+    $(document).on('click', '#aboutPage', function () {
+        window.location = "./about.html";
+    });
 });
 function readForm() {
+    cvm.schedules.removeAll();
     var courseTitle = $('#courseTitle').val();
-    var courseID = $('#courseID').val();
-    var startTime = $('#courseStart').val();
-    var endTime = $('#courseEnd').val();
-    var courseDays = $('#courseDays').val().toUpperCase();
-    if (courseID == "" || startTime == "" || endTime == "") {
-        alert("Please Fill out the required fields");
+    for (var i = 0; i < coursesArray.length; i++) {
+        if (coursesArray[i].TITLE == courseTitle) {
+            console.log(coursesArray[i].TITLE);
+
+        }
     }
-    else {
-        cvm.courses.push(new course(courseTitle, courseID, startTime, endTime, courseDays));
-        closeForm();
+    cvm.courseList.push(courseTitle);
+    console.log(cvm.courseList);
+    var result = solve(cvm.courseList);
+    console.log(result);
+
+
+    for (var index = 0; index < result.length; index++) {
+        var newSchedule = new scheduleObject();
+        var cs = result[index].id.split(',');
+        for (var x = 0; x < cs.length; x++) {
+
+            console.log(":" + $.trim(cs[x]) + ":");
+            var csn = $.trim(cs[x]);
+            newSchedule.courses().push(lookup(csn));
+            console.log(lookup(csn));
+        }
+        cvm.schedules.push(newSchedule);
+
+    }
+    console.log(newSchedule.courses().length);
+    /* var courseID = $('#courseID').val();
+     var startTime = $('#courseStart').val();
+     var endTime = $('#courseEnd').val();
+     var courseDays = $('#courseDays').val().toUpperCase();
+     if (courseID == "" || startTime == "" || endTime == "") {
+         alert("Please Fill out the required fields");
+     }
+     else {*/
+    //cvm.courses.push(new course(courseTitle, courseID, startTime, endTime, courseDays));
+
+
+    console.log(cvm.schedules().length);
+    closeForm();
+    //}
+}
+
+function computeSchedule() {
+    for (var i = 0; i < coursesArray.length; i++) {
+        if (coursesArray[i].TITLE == courseTitle) {
+            console.log(coursesArray[i].TITLE);
+
+        }
     }
 }
+
+
 function showForm() {
     formOpen(true);
 }
---
+++ Mobile/ScheduleBuilder/ScheduleBuilder/www/scripts/schedule.js
@@ -0,0 +1,138 @@
+// Finds schedules with the given title constraints
+
+
+// var titles = ['software engineering', 'compiler theory', 'software testing']; //, 'compiler theory'
+//var titles = ['Materials Laboratory', 'Intro to Human Factors', 'Advanced Human Factors', 'Aviation Security', 'Aviation Statistics']
+
+
+
+
+function cartesian(arg) {
+    var r = [],
+        max = arg.length - 1;
+
+    function helper(arr, i) {
+        for (var j = 0, l = arg[i].length; j < l; j++) {
+            var a = arr.slice(0); // clone arr
+            a.push(arg[i][j]);
+            if (i == max)
+                r.push(a);
+            else
+                helper(a, i + 1);
+        }
+    }
+    helper([], 0);
+    return r;
+}
+
+function dateRangeOverlaps(a_start, a_end, b_start, b_end) {
+    if (a_start <= b_start && b_start <= a_end) return true; // b starts in a
+    if (a_start <= b_end && b_end <= a_end) return true; // b ends in a
+    if (b_start < a_start && a_end < b_end) return true; // a in b
+    return false;
+}
+
+function multipleDateRangeOverlaps(arguments) {
+    var i, j;
+    if (arguments.length % 2 !== 0)
+        throw new TypeError('Arguments length must be a multiple of 2');
+    for (i = 0; i < arguments.length - 2; i += 2) {
+        for (j = i + 2; j < arguments.length; j += 2) {
+            if (
+                dateRangeOverlaps(
+                    arguments[i], arguments[i + 1],
+                    arguments[j], arguments[j + 1]
+                )
+            ) return true;
+        }
+    }
+    return false; //no overlap
+}
+
+function solve(titles) {
+    var options = [];
+    var schedule = [];
+    var stationary = {};
+    for (i = 0; i < titles.length; i++) {
+        stationary[titles[i]] = [];
+    }
+
+    var db = coursesArray;
+    for (i = 0; i < titles.length; i++) {
+        for (j = 0; j < db.length; j++) {
+            if (titles[i] == db[j].TITLE) {
+                stationary[titles[i]].push(db[j]);//id
+                // options.push(db[j]);
+            }
+        }
+    }
+
+    for (var i in stationary) {
+        options.push(stationary[i]);
+    }
+    options = cartesian(options)
+
+    var time1 = []; //monday
+    var time2 = [];
+    var time3 = [];
+    var time4 = [];
+    var time5 = [];
+    var time6 = []; //saturday
+    var flag = true;
+    var solutions = [];
+
+    for (var o in options) { //0 ,1, 2 
+        //each possible schedule	
+        for (var m in options[o]) {
+
+            for (v in options[o][m].occu) {
+                if (options[o][m].occu[v].days.indexOf('M') > -1) {
+                    time1.push(options[o][m].occu[v].st);
+                    time1.push(options[o][m].occu[v].end);
+                }
+                if (options[o][m].occu[v].days.indexOf('T') > -1) {
+                    time2.push(options[o][m].occu[v].st);
+                    time2.push(options[o][m].occu[v].end);
+                }
+                if (options[o][m].occu[v].days.indexOf('W') > -1) {
+                    time3.push(options[o][m].occu[v].st);
+                    time3.push(options[o][m].occu[v].end);
+                }
+                if (options[o][m].occu[v].days.indexOf('R') > -1) {
+                    time4.push(options[o][m].occu[v].st);
+                    time4.push(options[o][m].occu[v].end);
+                }
+                if (options[o][m].occu[v].days.indexOf('F') > -1) {
+                    time5.push(options[o][m].occu[v].st);
+                    time5.push(options[o][m].occu[v].end);
+                }
+                if (options[o][m].occu[v].days.indexOf('S') > -1) {
+                    time6.push(options[o][m].occu[v].st);
+                    time6.push(options[o][m].occu[v].end);
+                }
+            }
+            solutions.push(options[o][m].CRN);
+        }
+        flag = !multipleDateRangeOverlaps(time1) &&
+            !multipleDateRangeOverlaps(time2) && !multipleDateRangeOverlaps(time3) &&
+            !multipleDateRangeOverlaps(time4) && !multipleDateRangeOverlaps(time5) && !multipleDateRangeOverlaps(time6);
+        //check time
+        if (flag) {
+            solutions = solutions +' ';
+            schedule.push({id:solutions});
+        }
+
+        time1 = [];
+        time2 = [];
+        time3 = [];
+        time4 = [];
+        time5 = [];
+        time6 = [];
+        solutions = [];
+    }
+    return schedule;
+
+}
+
+//var s = solve(db, titles);
+//console.log(s)
